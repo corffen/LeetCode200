a6189138e2973ba4ed8bf460f4d95b1c507039ab
corffen <corffen@gmail.com>
2022-08-31 22:49:30 +0800

    替换字符串中的空格

--- src/main/java/com/gordon/string/StringDemo.java
+++ src/main/java/com/gordon/string/StringDemo.java
@@ -6,14 +6,61 @@ public class StringDemo {
         int l = 0;
         int r = s.length - 1;
         while (l < r) {
-            swap(s,l,r);
+            swap(s, l, r);
             l++;
             r--;
         }
     }
-    private void swap(char[] s,int p,int q){
+
+    private void swap(char[] s, int p, int q) {
         char temp = s[p];
         s[p] = s[q];
         s[q] = temp;
+    }
+
+    public String reverseStr(String s, int k) {
+        char[] charArray = s.toCharArray();
+        for (int i = 0, n = charArray.length; i < n; i += 2 * k) {
+            int start = i;
+            int end = Math.min(n - 1, start + k - 1);
+            while (start < end) {
+                swap(charArray, start, end);
+                start++;
+                end--;
+            }
+        }
+        return new String(charArray);
+    }
+
+    //替换字符串中的空格
+    public String replaceSpace(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                sb.append("  ");
+            }
+        }
+        if (sb.length() == 0) {
+            return s;
+        }
+        int left = s.length() - 1;
+        s += sb.toString();
+        int right = s.length() - 1;
+        char[] chars = s.toCharArray();
+        while (left >= 0) {
+            if (chars[left] == ' ') {
+                chars[right--] = '0';
+                chars[right--] = '2';
+                chars[right] = '%';
+            } else {
+                chars[right] = chars[left];
+            }
+            left--;
+            right--;
+        }
+        return new String(chars);
     }
 }
