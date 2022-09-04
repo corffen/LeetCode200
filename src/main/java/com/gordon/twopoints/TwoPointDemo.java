9ae67108040e55fdbe5cc228f3b0383197599aba
gordon_kara <corffen@gmail.com>
2022-09-04 22:27:32 +0800

    反转字符串

--- src/main/java/com/gordon/twopoints/TwoPointDemo.java
+++ src/main/java/com/gordon/twopoints/TwoPointDemo.java
@@ -15,4 +15,16 @@ public class TwoPointDemo {
         }
         return slow;
     }
+
+    public void reverseString(char[] s) {
+        int l = 0;
+        int r = s.length-1;
+        while (l < r) {
+            char temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+    }
 }
