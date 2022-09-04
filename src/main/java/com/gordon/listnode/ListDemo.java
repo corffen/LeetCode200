ad98e60d0cfc0c403bf2768f4038292aefdc264c
corffen <corffen@gmail.com>
2022-09-05 07:55:42 +0800

    删除链表中的元素,翻转链表

--- src/main/java/com/gordon/listnode/ListDemo.java
+++ src/main/java/com/gordon/listnode/ListDemo.java
@@ -1,6 +1,44 @@
 package com.gordon.listnode;
 
 public class ListDemo {
+    /**
+     * 移除链表中的元素
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode temp = dummy;
+        while (temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    /**
+     * 翻转链表,创建当前节点和前一个节点,指向关系,逐步拼接
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        ListNode temp = null;
+        while(curr != null) {
+            temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+        return pre;
+    }
+
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode fast = head;
         while (n-- > 0) {
