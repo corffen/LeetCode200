7f2b026fa4422fb426dd3239eb4a6b4b3df038cd
corffen <corffen@gmail.com>
2022-09-20 21:04:23 +0800

    迭代法求二叉树的最大高度

--- src/main/java/com/gordon/binary/tree/BinaryTreeDemo.java
+++ src/main/java/com/gordon/binary/tree/BinaryTreeDemo.java
@@ -399,4 +399,35 @@ public class BinaryTreeDemo {
         }
         return true;
     }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return depth;
+    }
 }
