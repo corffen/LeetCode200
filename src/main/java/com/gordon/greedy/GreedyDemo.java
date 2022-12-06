b47a45c6bb827ba5c0963bc96cb9301c59193c76
corffen <corffen@gmail.com>
2022-12-06 23:12:34 +0800

    贪心算法之摆动序列

--- src/main/java/com/gordon/greedy/GreedyDemo.java
+++ src/main/java/com/gordon/greedy/GreedyDemo.java
@@ -25,4 +25,27 @@ public class GreedyDemo {
         }
         return result;
     }
+
+    /**
+     * 摆动序列
+     * 获取一个数组中的摆动序列的最长子序列的长度
+     * @param nums
+     * @return
+     */
+    public int wiggleMaxLength(int[] nums) {
+        if (nums.length <= 1) {
+            return nums.length;
+        }
+        int result = 1;
+        int currentDiff = 0;
+        int preDiff = 0;
+        for (int i = 0; i < nums.length - 1; i++) {
+            currentDiff = nums[i + 1] - nums[i];
+            if ((currentDiff > 0 && preDiff <= 0) || (currentDiff < 0 && preDiff >= 0)) {
+                result++;
+                preDiff = currentDiff;
+            }
+        }
+        return result;
+    }
 }
