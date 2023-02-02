a443be88d30215175f6e0e00ace8f0aa1c3b5429
corffen <corffen@gmail.com>
2023-02-02 21:33:12 +0800

    动态规划,打家劫舍II

--- src/main/java/com/gordon/dp/DpDemo3.java
+++ src/main/java/com/gordon/dp/DpDemo3.java
@@ -17,4 +17,30 @@ public class DpDemo3 {
         }
         return dp[len - 1];
     }
+
+    /**
+     * <a href="https://leetcode.cn/problems/house-robber-ii/">213. 打家劫舍 II</a>
+     * 打家劫舍,收尾相邻的解法
+     * @param nums
+     * @return
+     */
+    public int rob2(int[] nums) {
+        int len = nums.length;
+        if (len == 1) {
+            return nums[0];
+        }
+        if (len == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robMax(nums, 0, len - 1), robMax(nums, 1, len));
+    }
+    private int robMax(int[] nums,int start,int end) {
+        int x = 0,y,z=0;
+        for (int i = start; i < end; i++) {
+            y=z;
+            z = Math.max(y, x + nums[i]);
+            x = y;
+        }
+        return z;
+    }
 }
