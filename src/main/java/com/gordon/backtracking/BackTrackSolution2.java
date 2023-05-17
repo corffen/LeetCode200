9b7524b2736a0722625c9e423970f853493274c1
Gordon-高枫 <gordon@hellotalk.com>
2023-05-17 10:25:13 +0800

    还原IP地址

--- src/main/java/com/gordon/backtracking/BackTrackSolution2.java
+++ src/main/java/com/gordon/backtracking/BackTrackSolution2.java
@@ -8,21 +8,23 @@ public class BackTrackSolution2 {
         bt2(candidates, target, 0, 0);
         return ans;
     }
+
     List<List<Integer>> ans = new ArrayList<>();
     LinkedList<Integer> path = new LinkedList<>();
-    private void bt2(int[] candidates, int target,int index,int sum){
+
+    private void bt2(int[] candidates, int target, int index, int sum) {
         if (sum == target) {
             ans.add(new ArrayList<>(path));
             return;
         }
         for (int i = index; i < candidates.length && sum + candidates[i] <= target; i++) {
             //注意这里是大于index,而不是大于0
-            if (i>index&&candidates[i]==candidates[i-1]){
+            if (i > index && candidates[i] == candidates[i - 1]) {
                 continue;
             }
             sum += candidates[i];
             path.add(candidates[i]);
-            bt2(candidates,target,i+1,sum);
+            bt2(candidates, target, i + 1, sum);
             sum -= candidates[i];
             path.pollLast();
         }
@@ -30,37 +32,88 @@ public class BackTrackSolution2 {
 
     /**
      * 分割回文字符串
+     *
      * @param s
      * @return
      */
     public List<List<String>> partition(String s) {
-        bt(s,0);
+        bt(s, 0);
         return lists;
     }
 
     List<List<String>> lists = new ArrayList<List<String>>();
     Deque<String> deque = new ArrayDeque<String>();
 
-    private void bt(String s,int index){
-        if (index>=s.length()){
+    private void bt(String s, int index) {
+        if (index >= s.length()) {
             lists.add(new ArrayList<>(deque));
             return;
         }
         for (int i = index; i < s.length(); i++) {
-            if (isP(s,index,i)) {
-                deque.add(s.substring(index,i+1));
-            }else {
+            if (isP(s, index, i)) {
+                deque.add(s.substring(index, i + 1));
+            } else {
                 continue;
             }
-            bt(s,i+1);
+            bt(s, i + 1);
             deque.pollLast();
         }
     }
-    private boolean isP(String s,int l,int r){
-        while(l<r){
-            if (s.charAt(l)!=s.charAt(r)) return false;
+
+    private boolean isP(String s, int l, int r) {
+        while (l < r) {
+            if (s.charAt(l) != s.charAt(r)) return false;
             l++;
             r--;
+        }
+        return true;
+    }
+
+
+    public List<String> restoreIpAddresses(String s) {
+        btIp(s,0,0);
+        return ips;
+    }
+
+    List<String> ips = new ArrayList<String>();
+
+    private void btIp(String s, int index, int num) {
+        if (num == 3) {
+            if (isValid(s, index, s.length()-1)) {
+                ips.add(s);
+            }
+            return;
+        }
+        for (int i = index; i < s.length(); i++) {
+            if (isValid(s, index, i)) {
+                s = s.substring(0, i + 1) + "." + s.substring(i + 1);
+                num++;
+                btIp(s,i+2,num);
+                s = s.substring(0, i + 1) + s.substring(i + 2);
+                num--;
+            }else{
+                break;
+            }
+        }
+    }
+
+    private boolean isValid(String s, int l, int r) {
+        if (l > r) {
+            return false;
+        }
+        if (s.charAt(l)=='0' && l != r) {
+            return false;
+        }
+        int num = 0;
+        for (int i = l; i < r; i++) {
+            int c = s.charAt(i) - '0';
+            if (c > 9 || c < 0) {
+                return false;
+            }
+            num = 10 * num + c;
+            if (num > 255) {
+                return false;
+            }
         }
         return true;
     }
