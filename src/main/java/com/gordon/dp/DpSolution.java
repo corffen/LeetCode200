6d472b0ab2c19490804103705d786394ce165772
Gordon <corffen@163.com>
2023-05-25 14:31:23 +0800

    动态规划---不同路径II

--- src/main/java/com/gordon/dp/DpSolution.java
+++ src/main/java/com/gordon/dp/DpSolution.java
@@ -29,4 +29,34 @@ public class DpSolution {
         }
         return dp[m-1][n-1];
     }
+
+    /**
+     * 遇到障碍物了，他的dp值就是0
+     * @param obstacleGrid
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+
+        //如果在起点或终点出现了障碍，直接返回0
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n && obstacleGrid[0][j] == 0; j++) {
+            dp[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = (obstacleGrid[i][j] == 0) ? dp[i - 1][j] + dp[i][j - 1] : 0;
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
