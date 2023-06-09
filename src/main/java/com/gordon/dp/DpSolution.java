0ac2408527a8e7fa3eb5842fae2afd4c1935cbfc
Gordon <corffen@163.com>
2023-06-09 16:43:35 +0800

    动态规划---完全平方数

--- src/main/java/com/gordon/dp/DpSolution.java
+++ src/main/java/com/gordon/dp/DpSolution.java
@@ -211,4 +211,23 @@ public class DpSolution {
         }
         return dp[amount] == max ? -1 : dp[amount];
     }
+
+    /**
+     * 279. 完全平方数
+     * 给你一个整数 n ，返回 和为 n 的完全平方数的最少数量 。
+     *
+     * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。
+     * 例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+     */
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j*j <= i; j++) {
+                dp[i] = Math.min(dp[i],dp[i-j*j]+1);
+            }
+        }
+        return dp[n];
+    }
 }
